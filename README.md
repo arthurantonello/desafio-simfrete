5be90bb7ff34614f65c5dddf69ae7c209559235a
Arthur Antonello <arthur.antonello@hotmail.com>
2025-04-30 21:17:44 -0300

    Commit desafio 1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # desafio-simfrete
  
+ ## Como cheguei à solução:
+    Iniciei tentando entender como implementaria esse desafio respeitando o Big O e tentando a melhor opção, a primeira solução que pensei foi uma busca por árvore binária por ter visto na cadeira de Estrutura de Dados e que era relativamente rápida para buscar em maiores quantidades, porém ao pesquisar novamente sobre li que para dados que se sobrepõe, nesse caso um cep dentro de outro mais abrangente (M,60000000,60000500 | N,60000125,60000375) poderia dar problema, portanto fui na mais simples porém para a quantidade considerada de CEPs foi uma boa solução, a busca sequencial.
+
+    Quando estava implementando vi que, por ler em sequência, já que M vinha antes na ordem, ele estava desconsiderando que o N era um resultado melhor por ser mais específico nos intervalos, portanto uma solução foi ordenar os intervalos baseando-se no seu inicio, garantindo que o primeiro que apareceria seria o "melhor", fiz isso utilizando o sort, porém com x1 - x0 para priorizar intervalos menores. Um trade-off de eficiência para retornar um resultado mais específico.
+
+    Fontes:
+    https://hub.asimov.academy/tutorial/como-ler-a-partir-do-stdin-em-python/
+    https://www.digitalocean.com/community/tutorials/read-stdin-python
+    https://blogboard.io/blog/knowledge/python-sorted-lambda/
+    https://www.freecodecamp.org/news/lambda-sort-list-in-python/
---
+++ achar_cep.py
@@ -0,0 +1,49 @@
+import sys
+
+def main():
+    cidades = []
+
+    # Ler as cidades até encontrar o breakpoint
+    for linha in sys.stdin:
+        linha = linha.strip() # Para remover espaços e o \n do stdin
+        if linha == '--':
+            break
+        
+        infos = linha.split(',')
+        if len(infos) != 3: #Garante que possui as 3 infos
+            continue
+        nome, inicio, fim  = infos[0].strip(), infos[1].strip(), infos[2].strip()
+
+        # Assegura que inicio é menor que o fim
+        if inicio > fim:
+            inicio, fim = fim, inicio
+
+        cidades.append((int(inicio), int(fim), nome))
+
+    # Ordena por tamanho do intervalo (menor primeiro)
+    cidades.sort(key=lambda intervalo: intervalo[1] - intervalo[0])
+
+    # Caso queira manter a lista original inalterada, porém é menos eficiente
+    #cidades_ordenadas = sorted(cidades, key=lambda intervalo: intervalo[1] - intervalo[0])
+
+    # Ler o CEP alvo
+    cep_alvo = None
+    for linha in sys.stdin:
+        if linha: # Ignora linhas vazias
+            cep_alvo = int(linha.strip())
+            break
+
+    if cep_alvo is None:
+        print('Nenhum CEP informado após o "--" para pesquisa.')
+        return
+
+    # Encontra o cep no intervalo
+    for inicio, fim, nome in cidades:
+        if inicio <= cep_alvo <= fim:
+            print(f'O CEP alvo se encontra em {nome}.')
+            return
+
+    print('CEP não encontrado.')
+
+if __name__ == '__main__':
+    main()
